b5b8d5205c8c992fc6728eb0754a9d0d912e3d31
Sydney Adams <sydadamz21@gmail.com>
2024-07-06 15:22:11 -0400

    temp footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import '../app/globals.css';
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <>
+      <div className="footer bg-gray-200 h-10 p-4	text-black flex items-center justify-center">
+        <p>
+          © Copyright 2024, Snake River Woodworks
+        </p>
+      </div>
+    </>
   )
 }
